052c429f363af90133389a7c1f6e6b821fb1710b
Andrew Soares <andrewsoares1@gmail.com>
2022-03-09 12:17:55 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/andrew/springreact/model/Movie.java
@@ -0,0 +1,24 @@
+package com.andrew.springreact.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Double score;
+    private Integer count;
+    private String image;
+}
---
+++ backend/src/main/java/com/andrew/springreact/model/User.java
@@ -0,0 +1,17 @@
+package com.andrew.springreact.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+}
