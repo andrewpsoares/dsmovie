b7be31fd6b4cb2fd2302103ec82ec43dfeccac90
Andrew Soares <andrewsoares1@gmail.com>
2022-03-11 13:01:55 -0300

    Delete ScoreService

--- backend/src/main/java/com/andrew/dsmovie/controller/ScoreController.java
+++ backend/src/main/java/com/andrew/dsmovie/controller/ScoreController.java
@@ -2,21 +2,17 @@ package com.andrew.dsmovie.controller;
 
 import com.andrew.dsmovie.dto.MovieDto;
 import com.andrew.dsmovie.dto.ScoreDto;
-import com.andrew.dsmovie.service.MovieService;
-import com.andrew.dsmovie.service.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/scores")
 public class ScoreController {
-    @Autowired
-    private ScoreService scoreService;
+//    @Autowired
+//    private ScoreService scoreService;
 
-    @PutMapping()
-    public MovieDto saveScore(@RequestBody ScoreDto scoreDto){
-        return scoreService.saveScore(scoreDto);
-    }
+//    @PutMapping()
+//    public MovieDto saveScore(@RequestBody ScoreDto scoreDto){
+//        return scoreService.saveScore(scoreDto);
+//    }
 }
